2a353fd209784e10be63c8ace15d2226f3f72c03
VET <73408920+VirtualEntertainmentTV@users.noreply.github.com>
2021-01-13 04:03:13 -0500

    Add Unity Project

---
+++ Assets/Scripts/Helper Scripts/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager instance;
+
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+       
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    private void Start()
+    {
+        SpawnEnemy();
+    }
+
+    public void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+
+
+} // class
